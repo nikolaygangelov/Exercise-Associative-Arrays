4c1e0ec66b72f3f315ea6676f6ef306dc4b7a47e
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-05 19:41:44 +0200

    Add files via upload

---
+++ Orders.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace _3._Orders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+    using System.Linq;
+    class Orders
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> products = new Dictionary<string, List<double>>();
+            double totalPrice = 0;
+            
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "buy")
+            {
+                string[] inputParameters = input.Split();
+                string name = inputParameters[0];
+                double price = double.Parse(inputParameters[1]);
+                double quantity = double.Parse(inputParameters[2]);
+
+                if (!products.ContainsKey(name))
+                {
+                    products[name] = new List<double>();
+                    products[name].Add(price);
+                    products[name].Add(quantity);
+                }
+                else
+                {
+                    products[name][1] += quantity;
+                    
+                    if (products[name][0] != price)
+                    {
+                        products[name][0] = price;
+                    }
+
+                }
+            }
+
+            foreach (var kvp in products)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value[0] * kvp.Value[1]:f2}");
+            }
+        }
+    }
+}
