833112f6a9c9c55c7c58cfc5d33b756e2ae6ff93
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-05 15:13:19 +0200

    Add files via upload

---
+++ Count Chars in a String.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Linq;
+
+namespace _1._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> occurrences = new Dictionary<char, int>();
+            string input = Console.ReadLine();
+            foreach (char ch in input)
+            {
+                if (ch != ' ')
+                {
+                    if (occurrences.ContainsKey(ch))
+                    {
+                        occurrences[ch] = 0;
+                    }
+                    occurrences[ch]++;
+                }
+            }
+
+            foreach (var kvp in occurrences)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
