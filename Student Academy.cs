25a134ea9951de32083dbcfd3359d8c8cdf7329b
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-06 22:59:06 +0200

    Add files via upload

---
+++ Student Academy.cs
@@ -0,0 +1,36 @@
+﻿
+namespace _6._Student_Academy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students[studentName] = new List<double>();
+                }
+
+                students[studentName].Add(grade);
+            }
+
+
+            foreach (var kvp in students.Where(x => x.Value.Sum() / x.Value.Count >= 4.50))
+            {
+                Console.WriteLine($"{kvp.Key} –> {kvp.Value.Sum() / kvp.Value.Count:f2}");
+            }
+        }
+    }
+}
