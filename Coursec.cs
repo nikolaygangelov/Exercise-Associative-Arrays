c8050a896ac0254ea97c60fd8495b9ecc9d6a4ca
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-06 23:03:43 +0200

    Add files via upload

---
+++ Coursec.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace _5._Courses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+    using System.Linq;
+    class Courses
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] inputParameters = input.Split(" : ");
+                string courseName = inputParameters[0];
+                string studentName = inputParameters[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses[courseName] = new List<string>();
+                }
+
+                courses[courseName].Add(studentName);
+            }
+
+            foreach (var kvp in courses)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count}");
+                foreach (string studentName in kvp.Value)
+                {
+                    Console.WriteLine($"-- {studentName}");
+                }
+
+            }
+
+        }
+    }
+}
